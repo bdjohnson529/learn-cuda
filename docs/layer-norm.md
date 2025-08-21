d209876ed8e3f77ed292794811a9fa67e1353ee4
Ben Johnson <bdjohnson529@gmail.com>
2025-08-21 17:58:52 +0000

    boilerplate: layer norm

---
+++ docs/layer-norm.md
@@ -0,0 +1,4 @@
+# Layer normalization in CUDA
+
+This is a guide to building a layer normalization kernel in CUDA.
+
---
+++ layernorm_kernel.cu
@@ -0,0 +1,7 @@
+#include <torch/extension.h>
+#include <ATen/cuda/CUDAContext.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+
+/
